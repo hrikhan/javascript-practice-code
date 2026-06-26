644e08d3283983b2fe4f0fc2486d32aeb238fbe9
Hridoy404 <hridoykhan.2025.new@gmail.com>
2026-06-26 09:53:17 +0600

    added kav script

---
+++ variable.js
@@ -0,0 +1,18 @@
+//create variables
+const userId = 1;
+let userName = "hridoy";
+let age = 24;
+let isactive = true;
+
+//update value 
+
+userName = "khan";
+
+//type conversion
+
+const num = Number('10');
+const str = String(100);
+
+
+console.log(userId, userName, isactive, age);
+console.log(num, str);
